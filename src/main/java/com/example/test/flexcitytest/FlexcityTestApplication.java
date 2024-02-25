85919141f43b8a53e64e729372441bb19af8f3ef
charlychoplin <charly.choplin14@gmail.com>
2024-02-25 21:31:51 +0100

    init commit

---
+++ src/main/java/com/example/test/flexcitytest/FlexcityTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.flexcitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexcityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexcityTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test/flexcitytest/FlexcityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.flexcitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexcityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
